3825b9f55c120535df987d8d987f34e2c3d1c3da
abderrahmaneaamri <abderrahmaneaamri@gmail.com>
2023-04-10 11:22:44 +0000

    add user controller and merge category

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,7 +28,15 @@ Route::get("auth/login", [LoginController::class, "login"]);
 Route::get("auth/logout", [LogoutController::class, "logout"]);
 
 
-Route::get("/showcategory/{id}", [CategoryController::class, "show"]);
-Route::post("/createcategory", [CategoryController::class, "create"]);
-Route::post("/updatecategory/{id}", [CategoryController::class, "edit"]);
-Route::post("/deletecategory/{id}", [CategoryController::class, "destroy"]);
+Route::get("/categories", [CategoryController::class, "index"]);
+Route::get("/categories/{id}", [CategoryController::class, "show"]);
+Route::post("/categories", [CategoryController::class, "store"]);
+Route::post("/categories/{id}", [CategoryController::class, "edit"]);
+Route::delete("/categories/{id}", [CategoryController::class, "destroy"]);
+
+
+Route::get("/users", [UserController::class, "index"]);
+Route::get("/users/{id}", [UserController::class, "show"]);
+Route::post("/users", [UserController::class, "store"]);
+Route::post("/users/{id}", [UserController::class, "edit"]);
+Route::delete("/users/{id}", [UserController::class, "destroy"]);
