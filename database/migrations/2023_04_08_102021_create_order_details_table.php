ec662f2eac308fc4371cdf96826d0da0e209b83e
abderrahmaneaamri <abderrahmaneaamri@gmail.com>
2023-05-04 21:10:29 +0100

    Add Status Column To Order Table

--- database/migrations/2023_04_08_102021_create_order_details_table.php
+++ database/migrations/2023_04_08_102021_create_order_details_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->float('total');
+            $table->string('status')->nullable()->default('process');
             $table->timestamps();
         });
     }
